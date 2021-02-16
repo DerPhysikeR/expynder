cbe5e1168bb5a42a4c5edba4a772b341675c9652
Paul Reiter <reiter.paul@gmail.com>
2021-02-16 22:30:46 +0100

    Add custom generator to allow remembering of call parameters

--- expynder.py
+++ expynder.py
@@ -5,10 +5,28 @@ def expand(function):
     return Expander(function)
 
 
+class RememberingGenerator:
+    def __init__(self, function, generator, num_args, kwargs_keys):
+        self.generator = generator
+        self.function = function
+        self.num_args = num_args
+        self.kwargs_keys = kwargs_keys
+        self.parameters = None
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        self.parameters = next(self.generator)
+        return self.function(
+            *self.parameters[: self.num_args],
+            **{k: v for k, v in zip(self.kwargs_keys, self.parameters[self.num_args :])}
+        )
+
+
 class Expander:
     def __init__(self, function_to_expand):
         self.function = function_to_expand
-        self._parameters = None
 
     def __call__(self, *args, **kwargs):
         return self.function(*args, **kwargs)
@@ -16,60 +34,12 @@ class Expander:
     def expand(self, expander, *args, **kwargs):
         # inject arbitrary expander functions, which take args and kwargs, e.g., expand only args
         generator_object = expander(*(list(args) + list(kwargs.values())))
-        for params in generator_object:
-            self._parameters = params
-            yield self(
-                *params[:len(args)],
-                **{k: v for k, v in zip(kwargs.keys(), params[len(args):])}
-            )
+        return RememberingGenerator(
+            self.function, generator_object, len(args), kwargs.keys()
+        )
 
     def product(self, *args, **kwargs):
-        yield from self.expand(product, *args, **kwargs)
+        return self.expand(product, *args, **kwargs)
 
     def zip(self, *args, **kwargs):
-        yield from self.expand(zip, *args, **kwargs)
-
-
-# https://towardsdatascience.com/implementing-a-data-pipeline-by-chaining-python-iterators-b5887c2f0ec6
-
-# @expand
-# def add(a, b):
-#     return a + b
-
-# def counter_factory(n):
-#     for i in range(n):
-#         yield i
-
-# if __name__ == "__main__":
-
-    # counter = counter_factory(10)
-    # counter.expynder = 5
-
-    # for count in counter:
-    #     print(counter.expynder)
-
-    # print(add(1, 2))
-    # a = add.zip([1, 2, 3], [4, 5, 6])
-    # b = add.product([1, 2, 3], [4, 5, 6])
-    # print(list(add.product(a, b)))
-
-    # print(list(add.zip([1, 2], [3, 4])))
-    # print(list(add.product([1, 2], [3, 4])))
-    # print(list(add.product([5, 7], add.product([1, 2], [3, 4]))))
-
-    # how to attach attribute to generator object?
-    # This would allow nesting of Expanders without modifying anything about the result.
-    # for sum_ in (exp := add.product([1, 2], [3, 4])):
-    #     print(add.params)
-    #     print(add.args)
-    #     exp.params
-
-    # I frequently perform parameter studies. Doing that with a fixed amount of
-    # parameters and functions is relatively straight forward, but what if each
-    # function exection had a different number of parameters and additionally
-    # multiple functions are nestedly called?
-    # How do you then get the parameters for each function call? How do you
-    # generate them in advance?
-    # `expynder` combines those two steps into one and makes them easy
-    # to perform.
-
+        return self.expand(zip, *args, **kwargs)
--- test_expynder.py
+++ test_expynder.py
@@ -1,29 +1,22 @@
 from expynder import expand
 
+
 @expand
 def add(a, b):
     return a + b
 
 
 def test_product():
-    assert tuple(add.product([1, 2], [3, 4])) == tuple([4, 5, 5, 6])
+    assert tuple(add.product([1, 2], [3, 4])) == (4, 5, 5, 6)
+
 
 def test_zip():
-    assert tuple(add.zip([1, 2], [3, 4])) == tuple([4, 6])
+    assert tuple(add.zip([1, 2], [3, 4])) == (4, 6)
 
-# def test_parameters():
-#     generator = add.product([1, 2], [3, 4])
-#     generator.someattribute = 5
-#     for result in generator:
-#         print(result)
-#         # breakpoint()
-#         print(add._parameters)
-#         # generator.parameters
-#         # generator.args ?
-#         # generator.kwargs ?
 
-# take a look at this tqdm example on how to get a handle on the generator itself
-# pbar = tqdm(["a", "b", "c", "d"])
-# for char in pbar:
-#     sleep(0.25)
-#     pbar.set_description("Processing %s" % char)
+def test_parameters():
+    results = (4, 5, 5, 6)
+    parameters = ((1, 3), (1, 4), (2, 3), (2, 4))
+    for i, result in enumerate(gen := add.product([1, 2], [3, 4])):
+        assert result == results[i]
+        assert gen.parameters == parameters[i]
